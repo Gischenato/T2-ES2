592e117994d6909cc1c96738e68373f466432e73
giovanni <gischenato@gmail.com>
2023-06-26 10:33:40 -0300

    changing package name

--- containers/src/main/java/br/pucrs/containers/errors/StudentAlreadyRegisteredException.java
+++
@@ -1,7 +0,0 @@
-package br.pucrs.containers.errors;
-
-public class StudentAlreadyRegisteredException extends RuntimeException {
-    public StudentAlreadyRegisteredException(String message) {
-        super(message);
-    }
-}
--- containers/src/main/java/br/pucrs/containers/errors/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.pucrs.containers.errors;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
