458371449e95ea82a69dca27b1bce8b50c9cb4cb
giovanni <gischenato@gmail.com>
2023-06-27 10:59:48 -0300

    removing comments

--- discipline-service/src/test/java/br/pucrs/discipline_service/DisciplineControllerTest.java
+++ discipline-service/src/test/java/br/pucrs/discipline_service/DisciplineControllerTest.java
@@ -14,7 +14,6 @@ import br.pucrs.discipline_service.errors.DisciplineNotFoundException;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
@@ -52,6 +51,7 @@ public class DisciplineControllerTest {
 
         verify(disciplineService, times(1)).registerDiscipline(any(Discipline.class));
     }
+  
     @Test
     public void testRegisterDisciplineAlreadyExists() throws Exception {
         doThrow(new DisciplineAlreadyRegisteredException("Discipline already registered"))
@@ -63,7 +63,6 @@ public class DisciplineControllerTest {
             .andExpect(status().isBadRequest())
             .andExpect(content().string("Discipline already registered"));
     }
-
 
     @Test
     public void testGetAllDisciplines() throws Exception {
@@ -113,36 +112,4 @@ public class DisciplineControllerTest {
             .andExpect(status().isNotFound());
     }
 
-    // @Test
-    // public void testGetAllDisciplinesByClassCodeAndTurma() throws Exception {
-    //     ClassCodeAndTurmaDTO dto1 = new ClassCodeAndTurmaDTO("classCode1", "turma1");
-    //     ClassCodeAndTurmaDTO dto2 = new ClassCodeAndTurmaDTO("classCode2", "turma2");
-
-    //     List<ClassCodeAndTurmaDTO> dtos = Arrays.asList(dto1, dto2);
-
-    //     Discipline discipline1 = Discipline.builder()
-    //     .id(1L).name("name1").classCode("classCode1").turma("turma1").build();
-
-    //     Discipline discipline2 = Discipline.builder()
-    //     .id(2L).name("name2").classCode("classCode2").turma("turma2").build();
-
-    //     List<Discipline> expectedDisciplines = Arrays.asList(discipline1, discipline2);
-
-    //     when(disciplineService.findAllByClassCodeAndTurma(dtos)).thenReturn(expectedDisciplines);
-
-    //     mockMvc.perform(post("/discipline/find/bulk")
-    //         .contentType(MediaType.APPLICATION_JSON)
-    //         .content("[{\"classCode\":\"classCode1\",\"turma\":\"turma1\"},"
-    //                 + "{\"classCode\":\"classCode2\",\"turma\":\"turma2\"}]"))
-    //         .andExpect(status().isOk())
-    //         .andExpect(jsonPath("$", hasSize(2)))
-    //         .andExpect(jsonPath("$[0].id", is(1)))
-    //         .andExpect(jsonPath("$[0].name", is("name1")))
-    //         .andExpect(jsonPath("$[0].classCode", is("classCode1")))
-    //         .andExpect(jsonPath("$[0].turma", is("turma1")))
-    //         .andExpect(jsonPath("$[1].id", is(2)))
-    //         .andExpect(jsonPath("$[1].name", is("name2")))
-    //         .andExpect(jsonPath("$[1].classCode", is("classCode2")))
-    //         .andExpect(jsonPath("$[1].turma", is("turma2")));
-    // }
 }
